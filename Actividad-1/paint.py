188888fb3beb6741a7211a864d98797fd9106e03
Felipe Barbosa <a01784465@tec.mx>
2024-09-11 17:40:57 -0600

    Modificacion en el archivo del juego 1

--- Actividad-1/paint.py
+++ Actividad-1/paint.py
@@ -1,29 +1,19 @@
-"""Paint, for drawing shapes.
-
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
-"""
-
 from turtle import *
-
 from freegames import vector
 
-
 def line(start, end):
-    """Draw line from start to end."""
+    """
+    Draw line from start to end.
+    """
     up()
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
 
-
 def square(start, end):
-    """Draw square from start to end."""
+    """
+    Draw square from start to end.
+    """
     up()
     goto(start.x, start.y)
     down()
@@ -35,24 +25,56 @@ def square(start, end):
 
     end_fill()
 
-
-def circle(start, end):
-    """Draw circle from start to end."""
-    pass  # TODO
-
+def circle_shape(start, end):
+    """
+    Draw a circle from start to end.
+    """
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    radius = ((end.x - start.x)**2 + (end.y - start.y)**2)**0.5  # Calculate radius
+    circle(radius)
+    end_fill()
 
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
-    pass  # TODO
+    """
+    Draw rectangle from start to end.
+    """
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    forward(end.x - start.x)
+    left(90)
+    forward(end.y - start.y)
+    left(90)
+    forward(end.x - start.x)
+    left(90)
+    forward(end.y - start.y)
+    
+    end_fill()
 
 def triangle(start, end):
-    """Draw triangle from start to end."""
-    pass  # TODO
+    """
+    Draw triangle from start to end.
+    """
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    for _ in range(3):
+        forward(end.x - start.x)
+        left(120)  # 120 degrees for an equilateral triangle
+    
+    end_fill()
 
 def tap(x, y):
-    """Store starting point or draw shape."""
+    """
+    Store starting point or draw shape when tapped.
+    """
     start = state['start']
 
     if start is None:
@@ -63,15 +85,18 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
-
 def store(key, value):
-    """Store value in state at key."""
+    """
+    Store value in state at key.
+    """
     state[key] = value
 
-
+# Set up the initial state
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
+
+# Assign keyboard shortcuts for undo and changing colors
 listen()
 onkey(undo, 'u')
 onkey(lambda: color('black'), 'K')
@@ -79,9 +104,14 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('yellow'), 'Y')  # New color: yellow
+
+# Assign keyboard shortcuts for shapes
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
-onkey(lambda: store('shape', rectangle), 'r')
-onkey(lambda: store('shape', triangle), 't')
+onkey(lambda: store('shape', circle_shape), 'c')  # Circle drawing function
+onkey(lambda: store('shape', rectangle), 'r')  # Rectangle drawing function
+onkey(lambda: store('shape', triangle), 't')  # Triangle drawing function
+
+# Start the turtle graphics loop
 done()
